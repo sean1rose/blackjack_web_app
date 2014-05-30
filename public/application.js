f9500e0a489e4572f20895b08177bb727fa54fda
Sean Rose <sean1rose@gmail.com>
2014-05-30 09:49:38 -0700

    Move functions w/in doc-ready func

--- public/application.js
+++ public/application.js
@@ -1,14 +1,11 @@
 $(document).ready(function(){       // upon page load...
-  $(document).on("click", "#hit input", function(){ 
-    alert("Player Hit AJAX");                                                       
-    $.ajax({
-      type: "POST",
-      url: "/game/player/hit"                         // no additional 'data' {} parameter to submit
-    }).done(function(msg){                            // .done-method chained to ajax to handle the response we get back
-      $('#game').replaceWith(msg)                     // .done takes anonymous function, which takes msg as parameter; msg is the response object. Response is the game-template response returned form the server                                                    
-    });                                                //  -> replacing contents/html of game div/template w/ new content that's returned in msg. WHAT'S RETUNRED IN THE MSG???
-    return false;
-  });
+  playerHit();
+  playerStay();
+  dealerHit();
+  doubleDown();
+});
+
+
 
 function playerHit() {
   $(document).on("click", "#hit input", function(){ 
@@ -60,6 +57,3 @@ function doubleDown() {
     return false;
   });
 }
-
-});
-
